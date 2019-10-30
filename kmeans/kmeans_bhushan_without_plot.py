80e92313060c3ccf17551a1602818099815c9188
Bhushan Borole <37565807+bhushan-borole@users.noreply.github.com>
2019-10-30 08:57:47 +0530

    Added predict method

--- kmeans/kmeans_bhushan_without_plot.py
+++ kmeans/kmeans_bhushan_without_plot.py
@@ -12,6 +12,24 @@ def print_output(clusters, centroids, iteration):
     for ce, cl in zip(centroids, clusters):
         print(f'Centroid={centroids[ce]}: Cluster={clusters[cl]}')
 
+def update_centroids(dim):
+    for c in clusters:
+        if dim == 1:
+            centroids[c] = round(sum(clusters[c])/len(clusters[c]), 2)
+        else:
+            avg_x = round(sum( p[0] for p in clusters[c] ) / len(clusters[c]), 2)
+            avg_y = round(sum( p[1] for p in clusters[c] ) / len(clusters[c]), 2)
+            centroids[c] = (avg_x, avg_y)
+
+def update_clusters(data, dim):
+    for d in data:
+        if dim == 2:
+            dist = [euclid(d, centroids[c]) for c in centroids]
+            clusters[dist.index(min(dist))].append(d)
+        else:
+            dist = [abs(d-centroids[c]) for c in centroids]
+            clusters[dist.index(min(dist))].append(d)
+
 def kmeans(data, k=2):
     # check the data is 2D or 1D
     dim = 2 if isinstance(data[0], tuple) else 1
@@ -26,25 +44,13 @@ def kmeans(data, k=2):
             clusters[i] = []
 
         # classifying data into clusters
-        for d in data:
-            if dim == 2:
-                dist = [euclid(d, centroids[c]) for c in centroids]
-                clusters[dist.index(min(dist))].append(d)
-            else:
-                dist = [abs(d-centroids[c]) for c in centroids]
-                clusters[dist.index(min(dist))].append(d)
+        update_clusters(data, dim)
         
         # copying centroids into a variable for future check
         prev_centroids = copy.deepcopy(centroids)
 
         # updating centroids according to the new clusters
-        for c in clusters:
-            if dim == 1:
-                centroids[c] = round(sum(clusters[c])/len(clusters[c]), 2)
-            else:
-                avg_x = round(sum( p[0] for p in clusters[c] ) / len(clusters[c]), 2)
-                avg_y = round(sum( p[1] for p in clusters[c] ) / len(clusters[c]), 2)
-                centroids[c] = (avg_x, avg_y)
+        update_centroids(dim)
         
         print_output(clusters, centroids, iters)
         iters += 1
@@ -53,8 +59,15 @@ def kmeans(data, k=2):
         if prev_centroids == centroids:
             break
 
+def predict(pt):
+    dim = 2 if isinstance(pt, tuple) else 1
+    update_clusters([pt], dim)
+    update_centroids(dim)
+    print_output(clusters, centroids, 'final')
+
 
 if __name__ == '__main__':
     data_1d = [2, 4, 10, 12, 11, 3, 30, 25, 20]
     data_2d = [(1,1), (2,1), (4,3), (5,4)]
     kmeans(data_2d)
+    predict((5, 6))
